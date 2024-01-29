e8c3958312aea028a8714821c9d4cdec6edb6227
Raphaël Bigot <contact@raphael-bigot.info>
2024-01-29 20:40:01 +0100

    change BASE URL env for production

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import {
-  BrowserRouter,
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 import App from './components/App/App';
 import Error from './components/Error/Error';
 import UserSpace from './components/UserSpace/UserSpace';
 import GameSheet from './components/GameSheet/GameSheet';
 import store from './store';
+
 import './styles/index.scss';
 import Home from './components/Home/Home';
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 const router = createBrowserRouter([
   {
